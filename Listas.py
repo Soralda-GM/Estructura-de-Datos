d8d6dd5d767e93667fcc2ce1acbe989c689c5bae
Soralda-GM <230i0135@martineztorre.tecnm.mx>
2024-10-03 11:37:41 -0600

    Add files via upload

--- Listas.py
+++ Listas.py
@@ -1,1 +1,9 @@
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
 
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+thislist = [10, 5, 159, 48, 15]
+print(thislist)
